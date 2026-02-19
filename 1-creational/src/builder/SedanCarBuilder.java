23060e7442541ec84ddc9153cfcc7d2a2d8cbdc9
Martin1540 <martinstewart5494@gmail.com>
2026-02-19 20:44:49 +0000

    Did a bit more

--- 1-creational/src/builder/SedanCarBuilder.java
+++ 1-creational/src/builder/SedanCarBuilder.java
@@ -4,42 +4,42 @@ public class SedanCarBuilder implements CarBuilder{
 
     @Override
     public void buildBodyStyle() {
-        System.out.println("External dimensions: overall length (inches): 202.9, overall width (inches): 76.2, overall height (inches): 60.7, wheelbase (inches): 112.9, front track (inches): 65.3, rear track (inches): 65.5 and curb to curb turning circle (feet): 39.5");
+        String bodyStyle = "External dimensions: overall length (inches): 202.9, overall width (inches): 76.2, overall height (inches): 60.7, wheelbase (inches): 112.9, front track (inches): 65.3, rear track (inches): 65.5 and curb to curb turning circle (feet): 39.5";
     }
 
     @Override
     public void buildPower(){
-        System.out.println("285 hp @ 6,500 rpm; 253 ft lb of torque @ 4,000 rpm");
+        String power = "285 hp @ 6,500 rpm; 253 ft lb of torque @ 4,000 rpm";
     }
 
     @Override
     public void buildEngine() {
-        System.out.println("3.5L Duramax V 6 DOHC");
+        String engine = "3.5L Duramax V 6 DOHC";
     }
 
     @Override
     public void buildBreaks() {
-        System.out.println("Four-wheel disc brakes: two ventilated. Electronic brake distribution");
+        String breaks = "Four-wheel disc brakes: two ventilated. Electronic brake distribution";
     }
 
     @Override
     public void buildSeats() {
-        System.out.println("Front seat center armrest.Rear seat center armrest.Split-folding rear seats");
+        String seats = "Front seat center armrest.Rear seat center armrest.Split-folding rear seats";
     }
 
     @Override
     public void buildWindows() {
-        System.out.println("Laminated side windows.Fixed rear window with defroster");
+        String windows = "Laminated side windows.Fixed rear window with defroster";
     }
 
     @Override
     public void buildFuelType() {
-        System.out.println("Gasoline 19 MPG city, 29 MPG highway, 23 MPG combined and 437 mi. range");
+        String fuelType = "Gasoline 19 MPG city, 29 MPG highway, 23 MPG combined and 437 mi. range";
     }
 
     @Override
     public Car getCar(){
+        // TODO
         return null;
-
     }
 }
--- 1-creational/src/builder/SportsCarBuilder.java
+++ 1-creational/src/builder/SportsCarBuilder.java
@@ -4,17 +4,17 @@ public class SportsCarBuilder implements CarBuilder{
 
     @Override
     public void buildBodyStyle() {
-        // TODO: implement
+        String bodyStyle = "External dimensions: overall length (inches): 192.3, overall width (inches): 75.5, overall height (inches): 54.2, wheelbase (inches): 112.3, front track (inches): 63.7, rear track (inches): 64.1 and curb to curb turning circle (feet): 37.7";
     }
 
     @Override
     public void buildPower(){
-        // TODO: implement
+        String power = "323 hp @ 6,800 rpm; 278 ft lb of torque @ 4,800 rpm";
     }
 
     @Override
     public void buildEngine() {
-        // TODO: implement
+        String engine = "3.6L V 6 DOHC and variable valve timing";
     }
 
     @Override
